aca37194ad4cd9551937310f9f0891da120840a3
td <td@tddeMacBook-Air.local>
2022-05-05 11:16:35 +0800

    feat: feat: 优化文案

--- commands/cicd.js
+++ commands/cicd.js
@@ -32,7 +32,7 @@ module.exports = function () {
 
         // copy templatee cicd里面的文件
         await shell.cp('-R', path.resolve(__dirname, `../template/cicd/${type}/`), process.cwd() + '/');
-
+        spinner.start('🚀 cicd配置 初始化成功');
         shell.exit(0);
     }).catch(e => {
         console.log(e);
