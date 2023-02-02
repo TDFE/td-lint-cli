68d49815eb858e49f9bec558bae37fc288cedce4
zyj <zyj@zyjdeMacBook-Pro.local>
2023-02-02 11:39:52 +0800

    feat: 增加项目瘦身命令

---
+++ commands/slim.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+const webpack = require('webpack');
+const lodash = require('lodash');
+const shell = require('shelljs');
+const merge = require('webpack-merge');
+const fg = require('fast-glob');
+const ora = require('ora');
+const spinner = ora('Loading undead unicorns');
+
+const baseWebpackConfig = require(`${process.cwd()}/build/webpack.base.conf`);
+
+const allFiles = fg.sync([`${process.cwd()}/src/**/*.*`]);
+
+const filterFiles = ['src/index.ejs', 'src/index.js'];
+
+class Slim {
+    apply(compiler) {
+        compiler.hooks.afterEmit.tapAsync('Slim', (compilation, callback) => {
+            // 打包用到的所有文件
+            const assets = Array.from(compilation.fileDependencies);
+            // 过滤掉其他文件，保留src目录下的文件
+            const usedFiles = assets.reduce((total, item) => {
+                if (item.includes('/src/') && !item.includes('/node_modules/')) {
+                    total.push(item);
+                }
+                return total;
+            }, []);
+
+            if (Array.isArray(allFiles)) {
+                const unUsedFiles = allFiles.reduce((total, item) => {
+                    if (!usedFiles.includes(item) && !filterFiles.some((fileName) => item.includes(fileName))) {
+                        total.push(item);
+                    }
+                    return total;
+                }, []);
+
+                if (unUsedFiles.length) {
+                    compilation.warnings = ['以下文件文件没有被采用，请确认是否需要删除'].concat(unUsedFiles);
+                }
+            }
+
+            callback();
+        });
+    }
+}
+
+module.exports = function () {
+    try {
+        spinner.start('正在查询依赖，可能会花费数分钟，请稍后');
+        webpack(
+            merge(lodash.pick(baseWebpackConfig, ['entry', 'resolve', 'module']), {
+                mode: 'none',
+                plugins: [new Slim()]
+            }),
+            (err, stats) => {
+                if (err) {
+                    console.error(err);
+                    spinner.stop('错误');
+                    return;
+                }
+                const info = stats.toJson();
+
+                if (stats.hasWarnings()) {
+                    const warns = Array.isArray(info.warnings)
+                        ? info.warnings.map((i) => {
+                              return i.message && i.message.replace(`${process.cwd()}/`, '');
+                          })
+                        : [];
+                    const str = warns.join('\n');
+                    console.warn(str);
+                    spinner.succeed(`完成查找,共计${warns.length}个`);
+                    return;
+                }
+                console.log('当前src目录下所有文件都已经被使用');
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        shell.exit(1);
+    }
+};
